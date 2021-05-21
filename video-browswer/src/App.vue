53898e42bbf1ae066374939261fd94ff76cd49e5
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-05-21 15:21:26 -0400

    added prop validation and communcation between app.vue and videlist.vue

--- video-browswer/src/App.vue
+++ video-browswer/src/App.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
         <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList :videos="videos"></VideoList>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
 import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+
 const API_KEY = 'AIzaSyDBg-CynXDGpdK-oaJYOSaVOJwcsmUwf2g'
 export default {
     name: 'App', 
     components: {
-        SearchBar
+        SearchBar,
+        VideoList
+    },
+    data() {
+        return { videos: [] }
     },
     methods: {
         onTermChange(searchTerm) {
@@ -24,7 +31,7 @@ export default {
                     q: searchTerm
                 }
             }).then(response => {
-                console.log(response)
+                this.videos = response.data.items
             })
 
         }
